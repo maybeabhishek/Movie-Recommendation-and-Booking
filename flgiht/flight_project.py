5108b4c8ba1a7239d610da937ec159d90cc4cf34
crescent-igor <amritgupta1999@gmail.com>
2018-10-28 19:11:43 +0530

    Flight Completed

--- flgiht/flight_project.py
+++ flgiht/flight_project.py
@@ -6,67 +6,67 @@ from simple_settings import settings
 # db = sqlite3.connect('movie_data.db')
 
 
-# db.execute('''CREATE TABLE movies
+# db.execute('''CREATE TABLE Flights
 #          (ID INT PRIMARY KEY  NOT NULL,
-#          MOVIE_NAME TEXT NOT NULL,
-#          MOVIE_RATING INT NOT NULL);''')
+#          flight_name TEXT NOT NULL,
+#          flight_rating INT NOT NULL);''')
 #
-# db.execute('''CREATE TABLE projections
+# db.execute('''CREATE TABLE Journey
 #          (ID INT PRIMARY KEY NOT NULL,
-#          MOVIE_ID INT REFERENCES movies(id),
+#          flight_id INT REFERENCES Flights(id),
 #          PROJECTIONS_TYPE TEXT NOT NULL,
 #          PROJECTIONS_DATE  DATE NOT NULL,
-#          PROJECTION_TIME TEXT NOT NULL );''')
+#          journey_time TEXT NOT NULL );''')
 #
 # db.execute('''CREATE TABLE reservations
 #          (ID INT PRIMARY KEY NOT NULL,
 #          USERNAME TEXT NOT NULL,
-#          PROJECTION_ID INT REFERENCES projections(ID),
+#          journey_id INT REFERENCES Journey(ID),
 #          ROW  INT NOT NULL,
 #          COLUMN INT NOT NULL );''')
 
 
 
-# db.execute('''INSERT INTO MOVIES VALUES (1, 'Venom', 7)''')
-# db.execute('''INSERT INTO MOVIES VALUES (2, 'Iron Man', 8)''')
-# db.execute('''INSERT INTO MOVIES  VALUES (3, 'Spider-Man', 7)''')
-# db.execute('''INSERT INTO MOVIES VALUES (4, 'Black panther', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (5, 'Harry potter and the chamber of secrets', 7)''')
-# db.execute('''INSERT INTO MOVIES VALUES (6, 'Greatest showman', 7)''')
-# db.execute('''INSERT INTO MOVIES  VALUES (7, 'La la land', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (8, 'Titanic', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (9, 'The Avengers', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (10, 'Avengers age of ultron', 7)''')
-# db.execute('''INSERT INTO MOVIES VALUES (11, 'Thor', 7)''')
-# db.execute('''INSERT INTO MOVIES VALUES (12, 'Batman Arkham knight', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (13, 'Flash', 8)''')
-# db.execute('''INSERT INTO MOVIES VALUES (14, 'Justice League', 6)''')
+# db.execute('''INSERT INTO Flights VALUES (1, 'Venom', 7)''')
+# db.execute('''INSERT INTO Flights VALUES (2, 'Iron Man', 8)''')
+# db.execute('''INSERT INTO Flights  VALUES (3, 'Spider-Man', 7)''')
+# db.execute('''INSERT INTO Flights VALUES (4, 'Black panther', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (5, 'Harry potter and the chamber of secrets', 7)''')
+# db.execute('''INSERT INTO Flights VALUES (6, 'Greatest showman', 7)''')
+# db.execute('''INSERT INTO Flights  VALUES (7, 'La la land', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (8, 'Titanic', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (9, 'The Avengers', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (10, 'Avengers age of ultron', 7)''')
+# db.execute('''INSERT INTO Flights VALUES (11, 'Thor', 7)''')
+# db.execute('''INSERT INTO Flights VALUES (12, 'Batman Arkham knight', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (13, 'Flash', 8)''')
+# db.execute('''INSERT INTO Flights VALUES (14, 'Justice League', 6)''')
 # db.commit()
 
 
 
 
 
-# db.execute('''INSERT INTO PROJECTIONS VALUES (1, 1, '3d','2018-10-12', '0900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (2, 1, '2d', '2018-10-12', '1000')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (3, 2, '3d', '2018-10-12', '0800')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (4, 2, '2d', '2018-10-12', '1200')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (5, 3, '3d', '2018-10-12', '0900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (6, 4, '2d', '2018-10-12','1900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (7, 5, '3d', '2018-10-12', '2100')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (8, 6, '2d', '2018-10-01', '2200')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (9, 6, '3d', '2018-10-12', '1000')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (10, 7, '2d', '2018-10-12', '0800')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (11, 8, '2d', '2018-10-12', '1200')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (12, 8, '3d', '2018-10-12', '0900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (13, 9, '3d', '2018-10-12', '1900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (14, 10, '2d', '2018-10-12', '2100')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (15, 11, '3d', '2018-10-12', '2200')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (16, 12, '2d', '2018-10-12', '1000')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (17, 13, '2d', '2018-10-12', '0800')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (18, 13, '3d', '2018-10-12', '1200')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (19, 14, '3d', '2018-10-12', '0900')''')
-# db.execute('''INSERT INTO PROJECTIONS VALUES (20, 14, '3d', '2018-10-12', '1900')''')
+# db.execute('''INSERT INTO Journey VALUES (1, 1, '3d','2018-10-12', '0900')''')
+# db.execute('''INSERT INTO Journey VALUES (2, 1, '2d', '2018-10-12', '1000')''')
+# db.execute('''INSERT INTO Journey VALUES (3, 2, '3d', '2018-10-12', '0800')''')
+# db.execute('''INSERT INTO Journey VALUES (4, 2, '2d', '2018-10-12', '1200')''')
+# db.execute('''INSERT INTO Journey VALUES (5, 3, '3d', '2018-10-12', '0900')''')
+# db.execute('''INSERT INTO Journey VALUES (6, 4, '2d', '2018-10-12','1900')''')
+# db.execute('''INSERT INTO Journey VALUES (7, 5, '3d', '2018-10-12', '2100')''')
+# db.execute('''INSERT INTO Journey VALUES (8, 6, '2d', '2018-10-01', '2200')''')
+# db.execute('''INSERT INTO Journey VALUES (9, 6, '3d', '2018-10-12', '1000')''')
+# db.execute('''INSERT INTO Journey VALUES (10, 7, '2d', '2018-10-12', '0800')''')
+# db.execute('''INSERT INTO Journey VALUES (11, 8, '2d', '2018-10-12', '1200')''')
+# db.execute('''INSERT INTO Journey VALUES (12, 8, '3d', '2018-10-12', '0900')''')
+# db.execute('''INSERT INTO Journey VALUES (13, 9, '3d', '2018-10-12', '1900')''')
+# db.execute('''INSERT INTO Journey VALUES (14, 10, '2d', '2018-10-12', '2100')''')
+# db.execute('''INSERT INTO Journey VALUES (15, 11, '3d', '2018-10-12', '2200')''')
+# db.execute('''INSERT INTO Journey VALUES (16, 12, '2d', '2018-10-12', '1000')''')
+# db.execute('''INSERT INTO Journey VALUES (17, 13, '2d', '2018-10-12', '0800')''')
+# db.execute('''INSERT INTO Journey VALUES (18, 13, '3d', '2018-10-12', '1200')''')
+# db.execute('''INSERT INTO Journey VALUES (19, 14, '3d', '2018-10-12', '0900')''')
+# db.execute('''INSERT INTO Journey VALUES (20, 14, '3d', '2018-10-12', '1900')''')
 
 
 # db.commit()
@@ -78,18 +78,18 @@ from simple_settings import settings
 #     print("proj_id = ", row[0])
 #     print("row = ", row[1])
 #     print("col = ", row[2], "\n")
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (9, 6, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1000')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (10, 7, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0800')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (11, 8, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1200')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (12, 8, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0900')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (13, 9, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1900')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (14, 10, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '2100')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (15, 11, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '2200')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (16, 12, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1000')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (17, 13, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0800')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (18, 13, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1200')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (19, 14, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0900')
-# INSERT INTO "SYSTEM"."PROJECTIONS" ("ID", "MOVIE_ID", "TYPE", "MOVIEDATE", "TIME") VALUES (20, 14, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1900')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (9, 6, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1000')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (10, 7, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0800')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (11, 8, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1200')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (12, 8, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0900')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (13, 9, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1900')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (14, 10, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '2100')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (15, 11, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '2200')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (16, 12, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1000')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (17, 13, '2d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0800')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (18, 13, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1200')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (19, 14, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '0900')
+# INSERT INTO "SYSTEM"."Journey" ("ID", "flight_id", "TYPE", "MOVIEDATE", "TIME") VALUES (20, 14, '3d', TO_DATE('2018-10-12 06:20:13', 'YYYY-MM-DD HH24:MI:SS'), '1900')
 
 # db.close()
 
@@ -100,36 +100,36 @@ class DBCommunicator:
         self.cursor = cursor
         self.db = db
     def get_movies(self):
-        return self.cursor.execute('''SELECT id, movie_name, movie_rating
-                                        FROM Movies
-                                        ORDER BY movie_rating DESC''')
+        return self.cursor.execute('''SELECT id, flight_name, flight_rating
+                                        FROM Flights
+                                        ORDER BY flight_rating DESC''')
 
-    def get_projections(self, movie_id):
-        return self.cursor.execute('''SELECT Projections.id, projection_type, projection_time,
-                                         projection_date, movie_id, movie_name
-                                    FROM Projections
-                                    JOIN Movies
-                                    ON Projections.movie_id = Movies.id
-                                    WHERE movie_id = ?
-                                    ORDER BY projection_date''', (str(movie_id),))
+    def get_projections(self, flight_id):
+        return self.cursor.execute('''SELECT Journey.id, journey_type, journey_time,
+                                         journey_date, flight_id, flight_name
+                                    FROM Journey
+                                    JOIN Flights
+                                    ON Journey.flight_id = Flights.id
+                                    WHERE flight_id = ?
+                                    ORDER BY journey_date''', (str(flight_id),))
 
-    def get_projections_with_date(self, movie_id, date):
-        return self.cursor.execute('''SELECT Projections.id, projection_type, projection_time,
-                                             projection_date, movie_id, movie_name
-                                      FROM Projections
-                                      JOIN Movies
-                                      ON Projections.movie_id = Movies.id
-                                      WHERE movie_id = ? AND projection_date = ?
-                                      ORDER BY projection_date''', (str(movie_id), str(date)))
+    def get_projections_with_date(self, flight_id, date):
+        return self.cursor.execute('''SELECT Journey.id, journey_type, journey_time,
+                                             journey_date, flight_id, flight_name
+                                      FROM Journey
+                                      JOIN Flights
+                                      ON Journey.flight_id = Flights.id
+                                      WHERE flight_id = ? AND journey_date = ?
+                                      ORDER BY journey_date''', (str(flight_id), str(date)))
 
-    def get_revervations_for_projection(self, projection_id):
+    def get_revervations_for_projection(self, journey_id):
         return self.cursor.execute('''SELECT row, col
                                           FROM Reservations 
-                                          WHERE projection_id = ?''', (projection_id,))
+                                          WHERE journey_id = ?''', (journey_id,))
 
     def final_reservation(self, user, proj_id, row, col):
         self.cursor.execute('''INSERT INTO Reservations(
-                                  username, projection_id, row, col) VALUES(?,?,?,?)''', (user, proj_id, row, col))
+                                  username, journey_id, row, col) VALUES(?,?,?,?)''', (user, proj_id, row, col))
         self.db.commit()
 
 
@@ -138,17 +138,17 @@ class Controller:
     def __init__(self, db_communicator):
         self.db_communicator = db_communicator
 
-    def generate_movies_table(self):
-        table = PrettyTable(["id", "movie_name", "movie_rating"])
+    def generate_flights_table(self):
+        table = PrettyTable(["id", "flight_name", "flight_rating"])
         for row in self.db_communicator.get_movies():
-            table.add_row([row["id"], row["movie_name"], row["movie_rating"]])
+            table.add_row([row["id"], row["flight_name"], row["flight_rating"]])
 
         return table
 
-    def create_cinema(self, projection_id):
+    def create_cinema(self, journey_id):
         rows = 10
         cols = 10
-        db_data = self.db_communicator.get_revervations_for_projection(projection_id)
+        db_data = self.db_communicator.get_revervations_for_projection(journey_id)
         cinema = []
 
         row_headers = [" " if x == 0 else x for x in range(rows + 1)]
@@ -162,19 +162,19 @@ class Controller:
 
         return cinema
 
-    def generate_projections_table(self, movie_id, date):
+    def generate_journey_table(self, flight_id, date):
         if date is not None:
-            db_result = self.db_communicator.get_projections_with_date(movie_id, date)
+            db_result = self.db_communicator.get_projections_with_date(flight_id, date)
         else:
-            db_result = self.db_communicator.get_projections(movie_id)
+            db_result = self.db_communicator.get_projections(flight_id)
 
-        table = PrettyTable(["projection_id", "projection_type", "projection_time",
-                             "projection_date", "movie_id", "movie_name"])
+        table = PrettyTable(["journey_id", "journey_type", "journey_time",
+                             "journey_date", "flight_id", "flight_name"])
 
         for row in db_result:
-            table.add_row([row["id"], row["projection_type"], row["projection_time"],
-                           row["projection_date"], row["movie_id"],
-                           row["movie_name"]])
+            table.add_row([row["id"], row["journey_type"], row["journey_time"],
+                           row["journey_date"], row["flight_id"],
+                           row["flight_name"]])
 
         return table
 
@@ -185,8 +185,8 @@ class Controller:
 
         return table
 
-    def fin_reservation(self, user, projection_id, row, col):
-        self.db_communicator.final_reservation(user,projection_id, row, col)
+    def fin_reservation(self, user, journey_id, row, col):
+        self.db_communicator.final_reservation(user,journey_id, row, col)
 
 
 class CLI:
@@ -196,21 +196,21 @@ class CLI:
 
         self.__user_is_active = True
         self.commands = {
-            "show_movies": self.show_movies,
-            "show_projections": self.show_projections,
+            "show_flights": self.show_flights,
+            "show_journeys": self.show_journeys,
             "make_reservations": self.make_reservations,
             "exit": self.exit
         }
 
-    def show_movies(self, *args):
-        print(self.controller.generate_movies_table())
+    def show_flights(self, *args):
+        print(self.controller.generate_flights_table())
 
-    def show_projections(self, *args):
-        movie_id = args[0]
+    def show_journeys(self, *args):
+        flight_id = args[0]
         date = None
         if len(args) > 1:
             date = args[1]
-        print(self.controller.generate_projections_table(movie_id, date))
+        print(self.controller.generate_journey_table(flight_id, date))
 
     def show_reservations(self, data):
         print(self.controller.generate_reservations_table(data))
@@ -218,17 +218,17 @@ class CLI:
     def make_reservations(self, *args):
         username = input("Enter your name: ")
         number_of_tickets = int(input("Enter number of tickets: "))
-        self.show_movies()
-        movie_id = int(input("Enter movie id: "))
-        self.show_projections(movie_id)
-        projection_id = int(input("Enter projection id: "))
-        self.show_reservations(self.controller.create_cinema(projection_id))
+        self.show_flights()
+        flight_id = int(input("Enter flight id: "))
+        self.show_journeys(flight_id)
+        journey_id = int(input("Enter journey id: "))
+        self.show_reservations(self.controller.create_cinema(journey_id))
         # ask for ticket seats
 
         for i in range(number_of_tickets):
             ticketR = int(input("Enter Row: "))
             ticketC = int(input("Enter Column: "))
-            self.controller.fin_reservation(username, projection_id, ticketR, ticketC)
+            self.controller.fin_reservation(username, journey_id, ticketR, ticketC)
             print("The ticket is booked\n")
         # if finalize
         # insert query
@@ -263,7 +263,7 @@ class Validator:
 
 
 def main():
-    db = sqlite3.connect("movie_db.db")
+    db = sqlite3.connect("flight_db.db")
     db.row_factory = sqlite3.Row
     cursor = db.cursor()
 
